46156ca4f59315c3da4892b14f3f5f13688b6e81
sanao <62137820+sanao1006@users.noreply.github.com>
2022-06-20 11:09:44 +0900

    introduce "newtype" insted of "type"

--- stack_mv.hs
+++ stack_mv.hs
@@ -6,126 +6,108 @@ import Data.Primitive.MutVar
 import Control.Monad.ST
 import qualified Data.Vector.Unboxing as VU
 
-type Queue m a = MutVar m(VU.Vector a)
-type Stack m a = MutVar m (VU.Vector a)
+newtype Stack m a = Stack{stack :: MutVar(PrimState m)(VU.Vector a)}
 
-initQueue  :: (PrimMonad m, VU.Unboxable a) => m(Queue(PrimState m)a)
-initQueue = newMutVar VU.empty
-initStack ::(PrimMonad m, VU.Unboxable a) => m(Stack(PrimState m)a)
-initStack = newMutVar VU.empty
+initStack :: (PrimMonad m, VU.Unboxable a) => m(Stack m a)
+initStack = Stack <$> newMutVar(VU.empty)
 
-pushQueue :: (PrimMonad m, VU.Unboxable a) => Queue(PrimState m) a -> a -> m()
-pushQueue que x = modifyMutVar' que (`VU.snoc` x)
-pushStack ::(PrimMonad m, VU.Unboxable a) => Stack (PrimState m) a -> a -> m()
-pushStack st x = modifyMutVar' st (`VU.snoc` x)
+pushStack :: (PrimMonad m, VU.Unboxable a) => Stack m a -> a -> m()
+pushStack st x = modifyMutVar'(stack st)(`VU.snoc`x)
 
-popQueue :: (PrimMonad m,VU.Unboxable a) => Queue (PrimState m) a -> m()
-popQueue que = modifyMutVar' que VU.tail
-popStack :: (PrimMonad m,VU.Unboxable a) => Stack (PrimState m) a -> m()
-popStack st = modifyMutVar' st VU.init
+popStack ::(PrimMonad m, VU.Unboxable a) => Stack m a -> m()
+popStack st = modifyMutVar'(stack st)(VU.init)
 
-topStack :: (PrimMonad m, VU.Unboxable a) => Stack(PrimState m) a -> m a
-topStack st = VU.last <$> readMutVar st
-frontQueue :: (PrimMonad m, VU.Unboxable a) => Queue(PrimState m) a -> m a
-frontQueue que = VU.head <$> readMutVar que
-backQueue :: (PrimMonad m, VU.Unboxable a) => Queue(PrimState m) a -> m a
-backQueue que = VU.last <$> readMutVar que
+topStack :: (PrimMonad m, VU.Unboxable a) => Stack m a -> m a
+topStack st = VU.last<$>readMutVar(stack st)
 
+sizeStack :: (PrimMonad m, VU.Unboxable a) => Stack m a -> m Int
+sizeStack st = VU.length<$>readMutVar(stack st)
 
-sizeStack ::(PrimMonad m, VU.Unboxable a) => Stack(PrimState m) a -> m Int
-sizeStack st = VU.length <$> readMutVar st
-sizeQueue :: (PrimMonad m, VU.Unboxable a) => Queue(PrimState m) a -> m Int
-sizeQueue que = VU.length <$> readMutVar que
-
-
-emptyStack :: (PrimMonad m, VU.Unboxable a) => Stack(PrimState m) a -> m Bool
+emptyStack :: (PrimMonad m, VU.Unboxable a) => Stack m a -> m Bool
 emptyStack st = do
-  stack <- readMutVar st
-  if VU.null stack then pure True 
-  else pure False 
-emptyQueue :: (PrimMonad m, VU.Unboxable a) => Queue(PrimState m) a -> m Bool
+  stack' <- readMutVar(stack st)
+  if VU.null stack' then pure True
+  else pure False
+
+newtype Queue m a = Queue{queue :: MutVar (PrimState m)(VU.Vector a)}
+
+initQueue  :: (PrimMonad m, VU.Unboxable a) => m(Queue m a)
+initQueue = Queue <$> newMutVar VU.empty
+
+pushQueue :: (PrimMonad m, VU.Unboxable a) => Queue m a -> a -> m()
+pushQueue que x = modifyMutVar' (queue que) (`VU.snoc` x)
+
+popQueue :: (PrimMonad m,VU.Unboxable a) => Queue m a -> m()
+popQueue que = modifyMutVar' (queue que) VU.tail
+
+frontQueue :: (PrimMonad m, VU.Unboxable a) => Queue m a -> m a
+frontQueue que = VU.head <$> readMutVar (queue que)
+backQueue :: (PrimMonad m, VU.Unboxable a) => Queue m a -> m a
+backQueue que = VU.last <$> readMutVar (queue que)
+
+sizeQueue :: (PrimMonad m, VU.Unboxable a) => Queue m a -> m Int
+sizeQueue que = VU.length <$> readMutVar (queue que)
+
+emptyQueue :: (PrimMonad m, VU.Unboxable a) => Queue m a -> m Bool
 emptyQueue que = do
-  stack <- readMutVar que
-  if VU.null stack then pure True 
+  queue' <- readMutVar (queue que)
+  if VU.null queue' then pure True 
   else pure False 
 
-type Deque m a = MutVar m(Seq.Seq a)
+newtype Deque m a = Deque{deque :: MutVar(PrimState m)(Seq.Seq a)}
 
-initDeque :: (PrimMonad m) => m(Deque (PrimState m) a)
-initDeque = newMutVar (Seq.empty)
+initDeque :: (PrimMonad m) => m(Deque m a)
+initDeque = Deque <$> newMutVar (Seq.empty)
 {-# INLINE initDeque #-}
 
-deqSingleton :: (PrimMonad m) => a -> m(Deque(PrimState m)a)
-deqSingleton x = newMutVar(Seq.singleton x)
+deqSingleton :: (PrimMonad m) => a -> m(Deque m a)
+deqSingleton x = Deque <$> newMutVar(Seq.singleton x)
 {-# INLINE deqSingleton #-}
 
-addLast :: (PrimMonad m) => Deque(PrimState m)a -> a -> m()
-addLast deq x = modifyMutVar deq(flip(Seq.|>)x)
+addLast :: (PrimMonad m) => Deque m a -> a -> m()
+addLast deq x = modifyMutVar(deque deq)(flip(Seq.|>)x)
 {-# INLINE addLast #-}
 
-addFirst :: (PrimMonad m) => Deque(PrimState m)a -> a -> m()
-addFirst deq x = modifyMutVar deq((Seq.<|)x)
+addFirst :: (PrimMonad m) => Deque m a -> a -> m()
+addFirst deq x = modifyMutVar (deque deq)((Seq.<|)x)
 {-# INLINE addFirst #-}
 
-popLast :: (PrimMonad m) => Deque(PrimState m)a -> m()
-popLast deq = modifyMutVar deq(seqInit.Seq.viewr)
+popLast :: (PrimMonad m) => Deque m a -> m()
+popLast deq = modifyMutVar (deque deq)(seqInit.Seq.viewr)
 	where
     	seqInit(seqRest Seq.:> seqLast) = seqRest
 {-# INLINE popLast #-}
 
-popFirst :: (PrimMonad m) => Deque(PrimState m)a -> m()
-popFirst deq = modifyMutVar deq(seqTail.Seq.viewl)
+popFirst :: (PrimMonad m) => Deque m a -> m()
+popFirst deq = modifyMutVar (deque deq)(seqTail.Seq.viewl)
 	where
     	seqTail(seqTop Seq.:< seqRest) = seqRest
 {-# INLINE popFirst #-}
 
-peekFirst :: (PrimMonad m) => Deque(PrimState m)a -> m a
-peekFirst deq = seqHead . Seq.viewl <$> readMutVar deq 
+peekFirst :: (PrimMonad m) => Deque m a -> m a
+peekFirst deq = seqHead . Seq.viewl <$> readMutVar (deque deq) 
     where
     	seqHead(seqTop Seq.:< seqRest) = seqTop
 {-# INLINE peekFirst #-}
 
-peekLast :: (PrimMonad m) => Deque(PrimState m)a -> m a
-peekLast deq = seqLast . Seq.viewr <$> readMutVar deq 
+peekLast :: (PrimMonad m) => Deque m a -> m a
+peekLast deq = seqLast . Seq.viewr <$> readMutVar (deque deq) 
     where
     	seqLast(seqRest Seq.:> seqEnd) = seqEnd
 {-# INLINE peekLast #-}
 
-contains :: (PrimMonad m, Eq a) => Deque(PrimState m)a -> a -> m Bool
+contains :: (PrimMonad m, Eq a) => Deque m a -> a -> m Bool
 contains deq x = do
-	a <- readMutVar deq
-	case ((Seq.elemIndexL)x a) of
+	deq' <- readMutVar (deque deq)
+	case ((Seq.elemIndexL)x deq') of
 		Just _ -> pure True
 		Nothing -> pure False
 {-# INLINE contains #-}
 
-deqLen :: (PrimMonad m) => Deque(PrimState m)a -> m Int
-deqLen deq = Seq.length <$> readMutVar deq
+deqLen :: (PrimMonad m) => Deque m a -> m Int
+deqLen deq = Seq.length <$> readMutVar (deque deq)
 {-# INLINE deqLen #-}
 
-deqIns :: (PrimMonad m) => Deque(PrimState m) a -> Int -> a -> m()
-deqIns deq i x = modifyMutVar deq(Seq.insertAt i x)
+deqIns :: (PrimMonad m) => Deque m a -> Int -> a -> m()
+deqIns deq i x = modifyMutVar (deque deq)(Seq.insertAt i x)
 {-# INLINE deqIns #-}
-
-main :: IO ()
-main = do
-  a <- initStack ::IO(Stack(PrimState IO)Int)
-  pushStack a 100
-  pushStack a 200
-  pushStack a 10
-  popStack a
-  print =<< readMutVar a
-  popStack a
-  popStack a
-  print =<< emptyStack a
-  pushStack a 20
-  print =<< topStack a
-
-  
-  let stack_st = runST $ do
-      b <- initStack ::  ST s(Stack(PrimState(ST s))Int)
-      pushStack b 100
-      pushStack b 10
-      sizeStack b
-  print stack_st
-  
